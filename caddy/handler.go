e1859a601857a3fc29f0f283cdd25aea6987e8eb
Sebastian Mancke <s.mancke@tarent.de>
2017-05-03 16:51:17 +0200

    fixed prefix handling for github in caddy

--- caddy/handler.go
+++ caddy/handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/tarent/loginsrv/login"
 	_ "github.com/tarent/loginsrv/osiam"
 	"net/http"
+	"path"
 	"strings"
 )
 
@@ -26,8 +27,7 @@ func NewCaddyHandler(next httpserver.Handler, path string, loginHandler *login.H
 }
 
 func (h *CaddyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	if httpserver.Path(r.URL.Path).Matches(h.path) &&
-		strings.HasSuffix(r.URL.Path, "/login") {
+	if strings.HasPrefix(r.URL.Path, path.Join(h.path, "/login")) {
 		h.loginHandler.ServeHTTP(w, r)
 		return 0, nil
 	} else {
