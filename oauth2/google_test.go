c69ed819dd4b617c04857c61016bdffb91bd54be
Joe Harlow <joe@paybase.io>
2017-08-10 15:00:48 +0100

    run go fmt on files

--- oauth2/google_test.go
+++ oauth2/google_test.go
@@ -1,10 +1,10 @@
 package oauth2
 
 import (
-  . "github.com/stretchr/testify/assert"
-  "net/http"
-  "net/http/httptest"
-  "testing"
+	. "github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 var googleTestUserResponse = `{
@@ -35,19 +35,19 @@ var googleTestUserResponse = `{
 }`
 
 func Test_Google_getUserInfo(t *testing.T) {
-  server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    Equal(t, "secret", r.FormValue("access_token"))
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    w.Write([]byte(googleTestUserResponse))
-  }))
-  defer server.Close()
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Equal(t, "secret", r.FormValue("access_token"))
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write([]byte(googleTestUserResponse))
+	}))
+	defer server.Close()
 
-  googleAPI = server.URL
+	googleAPI = server.URL
 
-  u, rawJSON, err := providerGoogle.GetUserInfo(TokenInfo{AccessToken: "secret"})
-  NoError(t, err)
-  Equal(t, "test@gmail.com", u.Sub)
-  Equal(t, "test@gmail.com", u.Email)
-  Equal(t, "Testy Test", u.Name)
-  Equal(t, googleTestUserResponse, rawJSON)
+	u, rawJSON, err := providerGoogle.GetUserInfo(TokenInfo{AccessToken: "secret"})
+	NoError(t, err)
+	Equal(t, "test@gmail.com", u.Sub)
+	Equal(t, "test@gmail.com", u.Email)
+	Equal(t, "Testy Test", u.Name)
+	Equal(t, googleTestUserResponse, rawJSON)
 }
--- oauth2/provider_test.go
+++ oauth2/provider_test.go
@@ -10,12 +10,12 @@ func Test_ProviderRegistration(t *testing.T) {
 	NotNil(t, github)
 	True(t, exist)
 
-        google, exist := GetProvider("google")
-        NotNil(t, google)
-        True(t, exist)
+	google, exist := GetProvider("google")
+	NotNil(t, google)
+	True(t, exist)
 
 	list := ProviderList()
 	Equal(t, 2, len(list))
 	Equal(t, "github", list[0])
-        Equal(t, "google", list[1])
+	Equal(t, "google", list[1])
 }
