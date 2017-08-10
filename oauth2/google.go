0c402a7607db58a8206573bd9fbb278fcb804fb2
Joel Pickup <joel@paybase.io>
2017-08-10 13:02:14 +0100

    google implementation

---
+++ oauth2/google.go
@@ -0,0 +1,67 @@
+package oauth2
+
+import (
+  "encoding/json"
+  "fmt"
+  "github.com/tarent/loginsrv/model"
+  "io/ioutil"
+  "net/http"
+  "strings"
+)
+
+var googleAPI = "https://www.googleapis.com/plus/v1"
+
+func init() {
+  RegisterProvider(providerGoogle)
+}
+
+type GoogleUser struct {
+  DisplayName string
+  Emails []struct{
+      Value string
+  }
+  Image struct {
+      Url string
+  }
+}
+
+var providerGoogle = Provider{
+  Name:     "google",
+  AuthURL:  "https://accounts.google.com/o/oauth2/v2/auth",
+  TokenURL: "https://www.googleapis.com/oauth2/v4/token",
+  GetUserInfo: func(token TokenInfo) (model.UserInfo, string, error) {
+    gu := GoogleUser{}
+    url := fmt.Sprintf("%v/people/me?alt=json&access_token=%v", googleAPI, token.AccessToken)
+    resp, err := http.Get(url)
+
+    if err != nil {
+      return model.UserInfo{}, "", err
+    }
+
+    if !strings.Contains(resp.Header.Get("Content-Type"), "application/json") {
+      return model.UserInfo{}, "", fmt.Errorf("wrong content-type on google get user info: %v", resp.Header.Get("Content-Type"))
+    }
+
+    if resp.StatusCode != 200 {
+      return model.UserInfo{}, "", fmt.Errorf("got http status %v on google get user info", resp.StatusCode)
+    }
+
+    b, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      return model.UserInfo{}, "", fmt.Errorf("error reading google get user info: %v", err)
+    }
+
+    err = json.Unmarshal(b, &gu)
+    if err != nil {
+      return model.UserInfo{}, "", fmt.Errorf("error parsing google get user info: %v", err)
+    }
+
+    return model.UserInfo{
+      Sub:     gu.Emails[0].Value,
+      Picture: gu.Image.Url,
+      Name:    gu.DisplayName,
+      Email:   gu.Emails[0].Value,
+      Origin:  "google",
+    }, string(b), nil
+  },
+}
---
+++ oauth2/google_test.go
@@ -0,0 +1,53 @@
+package oauth2
+
+import (
+  . "github.com/stretchr/testify/assert"
+  "net/http"
+  "net/http/httptest"
+  "testing"
+)
+
+var googleTestUserResponse = `{
+  "kind": "plus#person",
+  "etag": "\"XX\"",
+  "gender": "male",
+  "emails": [
+    {
+      "value": "test@gmail.com",
+      "type": "account"
+    }
+  ],
+  "objectType": "person",
+  "id": "1",
+  "displayName": "Testy Test",
+  "name": {
+    "familyName": "Test",
+    "givenName": "Testy"
+  },
+  "url": "https://plus.google.com/X",
+  "image": {
+    "url": "https://lh3.googleusercontent.com/X/X/X/X/photo.jpg?sz=50",
+    "isDefault": true
+  },
+  "isPlusUser": true,
+  "circledByCount": 0,
+  "verified": false
+}`
+
+func Test_Google_getUserInfo(t *testing.T) {
+  server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    Equal(t, "secret", r.FormValue("access_token"))
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    w.Write([]byte(googleTestUserResponse))
+  }))
+  defer server.Close()
+
+  googleAPI = server.URL
+
+  u, rawJSON, err := providerGoogle.GetUserInfo(TokenInfo{AccessToken: "secret"})
+  NoError(t, err)
+  Equal(t, "test@gmail.com", u.Sub)
+  Equal(t, "test@gmail.com", u.Email)
+  Equal(t, "testy test", u.Name)
+  Equal(t, googleTestUserResponse, rawJSON)
+}
