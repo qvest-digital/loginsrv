fc5e3a317bacfc6a6e4c93a77486f2be66be0ea7
magikstm <myskina@gmail.com>
2017-11-09 00:09:14 -0500

    Add a test for /login

--- caddy/handler_test.go
+++ caddy/handler_test.go
@@ -45,3 +45,41 @@ func Test_ServeHTTP_200(t *testing.T) {
 		t.Errorf("Expected returned status code to be %d, got %d", 0, status)
 	}
 }
+
+func Test_ServeHTTP_login(t *testing.T) {
+	//Set the ServeHTTP *http.Request
+	r, err := http.NewRequest("GET", "/login", nil)
+	if err != nil {
+		t.Fatalf("Unable to create request: %v", err)
+	}
+	
+	//Set the ServeHTTP http.ResponseWriter
+	w := httptest.NewRecorder()
+	
+	//Set the CaddyHandler config
+	configh := login.DefaultConfig()
+	configh.Backends = login.Options{"simple": {"bob": "secret"}}
+	loginh, err := login.NewHandler(configh)
+	if err != nil {
+		t.Errorf("Expected nil error, got: %v", err)
+	}
+	
+	//Set the CaddyHandler that will use ServeHTTP
+	h := &CaddyHandler{
+	next: httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
+		return http.StatusOK, nil // not t.Fatalf, or we will not see what other methods yield
+		}),
+	config:       login.DefaultConfig(),
+	loginHandler: loginh,
+	}
+	
+	status, err := h.ServeHTTP(w, r)
+	
+	if err != nil {
+		t.Errorf("Expected nil error, got: %v", err)
+	}
+	
+	if status != 0 {
+		t.Errorf("Expected returned status code to be %d, got %d", 0, status)
+	}
+}
