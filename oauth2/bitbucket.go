032605f776e9230c04a5d44ffbcce3873bac173d
Andres De Castro <af.decastro879@gmail.com>
2017-11-13 13:12:43 +0200

    #51 | Added Pulling email information from the user using Bitbucket API

--- oauth2/bitbucket.go
+++ oauth2/bitbucket.go
@@ -10,7 +10,7 @@ import (
 )
 
 var bitbucketAPI = "https://api.bitbucket.org/2.0"
-// Using the avatar url to be able to fetch 128px image. By default Bitbucket API return 32px image.
+// Using the avatar url to be able to fetch 128px image. By default BitbucketAPI return 32px image.
 var bitbucketAvatarURL = "https://bitbucket.org/account/%v/avatar/128/"
 
 func init() {
@@ -19,9 +19,72 @@ func init() {
 
 // BitbucketUser is used for parsing the github response
 type BitbucketUser struct {
-	Username     string `json:"username,omitempty"`
-	DisplayName      string `json:"display_name,omitempty"`
-	Email     string `json:"email,omitempty"`
+	Username    string `json:"username,omitempty"`
+	DisplayName string `json:"display_name,omitempty"`
+	Email       string `json:"email,omitempty"`
+}
+
+// emails is used to parse user email information
+type emails struct {
+	Page    int `json:"page,omitempty"`
+	PageLen int `json:"pagelen,omitempty"`
+	Size    int `json:"size,omitempty"`
+	Values  []email
+}
+
+// email used to parse one user's email
+type email struct {
+	Email       string `json:"email,omitempty"`
+	IsConfirmed bool   `json:"is_confirmed,omitempty"`
+	IsPrimary   bool   `json:"is_primary,omitempty"`
+	Links struct {
+		Self struct {
+			Href string
+		}
+	} `json:"links,omitempty"`
+	Type string `json:"type,omitempty"`
+}
+
+// GetPrimaryEmailAddress retrieve the primary email address of the user
+func (e *emails) GetPrimaryEmailAddress() string {
+	for _, val := range e.Values {
+		if val.IsPrimary {
+			return val.Email
+		}
+	}
+	return ""
+}
+
+// getBitbucketEmails Retrieves bitbucket user emails hiting the Bitbucket API emails service
+func getBitbucketEmails(token TokenInfo) (emails, error) {
+	emailUrl := fmt.Sprintf("%v/user/emails?access_token=%v", bitbucketAPI, token.AccessToken)
+	userEmails := emails{}
+	resp, err := http.Get(emailUrl)
+
+	if err != nil {
+		return emails{}, err
+	}
+
+	if !strings.Contains(resp.Header.Get("Content-Type"), "application/json") {
+		return emails{}, fmt.Errorf("wrong content-type on bitbucket get user emails: %v", resp.Header.Get("Content-Type"))
+	}
+
+	if resp.StatusCode != 200 {
+		return emails{}, fmt.Errorf("got http status %v on bitbucket get user emails", resp.StatusCode)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return emails{}, fmt.Errorf("error reading bitbucket get user emails: %v", err)
+	}
+
+	err = json.Unmarshal(b, &userEmails)
+
+	if err != nil {
+		return emails{}, fmt.Errorf("error parsing bitbucket get user emails: %v", err)
+	}
+
+	return userEmails, nil
 }
 
 var providerBitbucket = Provider{
@@ -55,11 +118,13 @@ var providerBitbucket = Provider{
 			return model.UserInfo{}, "", fmt.Errorf("error parsing bitbucket get user info: %v", err)
 		}
 
+		userEmails, err := getBitbucketEmails(token)
+
 		return model.UserInfo{
 			Sub:     gu.Username,
 			Picture: fmt.Sprintf(bitbucketAvatarURL, gu.Username),
 			Name:    gu.DisplayName,
-			Email:   gu.Email,
+			Email:   userEmails.GetPrimaryEmailAddress(),
 			Origin:  "bitbucket",
 		}, string(b), nil
 	},
