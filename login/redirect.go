ca1748e8777b6ced583f74a89a8af649687ae03c
ptx <ptx@uber.com>
2019-03-02 11:43:52 +0100

    Added flag to allow redirect between subdomains

--- login/redirect.go
+++ login/redirect.go
@@ -49,11 +49,40 @@ func (h *Handler) allowRedirect(r *http.Request) bool {
 		logging.Application(r.Header).Warnf("couldn't parse redirect url %s", err)
 		return false
 	}
+
 	if referer.Host != r.Host {
 		logging.Application(r.Header).Warnf("redirect from referer domain: '%s', not matching current domain '%s'", referer.Host, r.Host)
 		return false
 	}
 	return true
+}
+
+func removeSubdomain(host string) string {
+	parts := strings.Split(host, ".")
+	if len(parts) == 1 {
+		return host
+	}
+	return strings.Join(parts[1:], ".")
+}
+
+// haveSubdomain checks that there's at least one subdomain
+func haveSubdomain(host string) bool {
+	trimmed := strings.Trim(host, ".")
+	parts := strings.Split(trimmed, ".")
+	return len(parts) > 2
+}
+
+func (h *Handler) isSubdomainAllowed(target string, host string) bool {
+	if !h.config.RedirectAllowSubdomain {
+		return false
+	}
+	if target == "" || host == "" {
+		return false
+	}
+	if !haveSubdomain(target) || !haveSubdomain(host) {
+		return false
+	}
+	return removeSubdomain(target) == removeSubdomain(host)
 }
 
 func (h *Handler) redirectURL(r *http.Request, w http.ResponseWriter) string {
@@ -62,6 +91,9 @@ func (h *Handler) redirectURL(r *http.Request, w http.ResponseWriter) string {
 		sameHost := targetURL.Host == "" || r.Host == targetURL.Host
 		if sameHost && targetURL.Path != "" {
 			return targetURL.Path
+		}
+		if h.isSubdomainAllowed(targetURL.Host, r.Host) {
+			return targetURL.String()
 		}
 		if !sameHost && h.isRedirectDomainWhitelisted(r, targetURL.Host) {
 			return targetURL.String()
