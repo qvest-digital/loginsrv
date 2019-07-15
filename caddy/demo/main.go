05f4f8659dde67db127b0a10ab499d9d2a746440
Andrew Parker <andrewmichaelparker@gmail.com>
2019-07-15 16:36:24 -0600

    Fix Caddy import paths
    The path change for Caddy import in version 1.0.1.

--- caddy/demo/main.go
+++ caddy/demo/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/BTBurke/caddy-jwt"
-	"github.com/mholt/caddy/caddy/caddymain"
+	"github.com/caddyserver/caddy/caddy/caddymain"
 	_ "github.com/tarent/loginsrv/caddy"
 )
 
